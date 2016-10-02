a79f486a6ea528612ae3c082978678dd7e141124
jgroehl <jgroehl@outlook.de>
2016-10-02 10:58:01 +0200

    included new placeholder icons. need some nice ones when releasing

--- app/src/main/java/de/asteromania/groehl/bloodpressurediary/domain/DataItemType.java
+++ app/src/main/java/de/asteromania/groehl/bloodpressurediary/domain/DataItemType.java
@@ -9,8 +9,8 @@ public enum DataItemType
 {
     SYSTOLE(R.string.dataTypeSystole, R.layout.activity_add_data_item_blood_pressure, R.string.unit_bloodPressure, R.mipmap.ic_launcher, InformationType.BLOOD_PRESSURE),
     DIASTOLE(R.string.dataTypeDiastole, R.layout.activity_add_data_item_blood_pressure, R.string.unit_bloodPressure, R.mipmap.ic_launcher, InformationType.BLOOD_PRESSURE),
-    HEARTRATE(R.string.dataTypeHeartRate, R.layout.activity_add_data_item, R.string.unit_heartrate, R.mipmap.ic_launcher, InformationType.HEARTRATE),
-    WEIGHT(R.string.dataTypeWeight, R.layout.activity_add_data_item, R.string.unit_weight, R.mipmap.ic_launcher, InformationType.WEIGHT);
+    HEARTRATE(R.string.dataTypeHeartRate, R.layout.activity_add_data_item, R.string.unit_heartrate, R.mipmap.ic_heartrate, InformationType.HEARTRATE),
+    WEIGHT(R.string.dataTypeWeight, R.layout.activity_add_data_item, R.string.unit_weight, R.mipmap.ic_weight, InformationType.WEIGHT);
 
     DataItemType(int text, int contentView, int unitString, int typeIcon, InformationType informationType)
     {
